0747b253fcac0ed0a9115fe545c0658cfb19e1bc
Hanna Serdjuk <92991892+Hanna-Serdjuk@users.noreply.github.com>
2022-05-09 14:18:02 +0300

    add vimeo and lodash + task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,3 +4,22 @@
 // при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
 
 
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+    player.on('timeupdate', throttle(currentTime, 1000)); 
+    
+    function currentTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log('current time: ', data.seconds);
+}
+
+const timeWatch = localStorage.getItem('videoplayer-current-time');
+
+    if (timeWatch) {
+    player.setCurrentTime(timeWatch);
+    localStorage.removeItem('videoplayer-current-time');
+    }
