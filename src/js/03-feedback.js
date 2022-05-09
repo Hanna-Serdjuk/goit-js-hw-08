825d6d4dda7acf91cd25b558098a1ab01e3cc845
Hanna Serdjuk <92991892+Hanna-Serdjuk@users.noreply.github.com>
2022-05-09 16:28:04 +0300

    add task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -21,15 +21,42 @@ const refs = {
     textArea: document.querySelector(".feedback-form textarea"),
 }
 
-const state = {};
+window.addEventListener('DOMContentLoaded', setFormValues);
 
-refs.feedbackForm.addEventListener('submit', (event) => {});
+const state = localStorage.getItem('feedback-form-state') 
+    ? JSON.parse(localStorage.getItem('feedback-form-state')) 
+    : {};
 
-refs.textArea.addEventListener('input', handleSetStorageState);
 
-refs.email.addEventListener('input', handleSetStorageState);
+refs.feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log({email: refs.email.value, message: refs.textArea.value});
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+
+});
+
+refs.textArea.addEventListener('input', throttle(handleSetStorageState, 500));
+
+refs.email.addEventListener('input', throttle(handleSetStorageState, 500));
 
 function handleSetStorageState(event) {
     state[event.target.name] = event.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(state));
+}
+
+function setFormValues() {
+
+    if(!localStorage.getItem('feedback-form-state'))
+    return;
+
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if(savedData.email) {
+        refs.email.value = savedData.email;
+    }
+
+    if(savedData.message) {
+        refs.textArea.value = savedData.message;
+    }
 }
