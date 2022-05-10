51e6f389bd4fb853d82e5a5b79847b0c2c34f119
Hanna Serdjuk <92991892+Hanna-Serdjuk@users.noreply.github.com>
2022-05-10 22:17:36 +0300

    fix task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,50 +13,47 @@
 // 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. 
 // Для этого добавь в проект и используй библиотеку lodash.throttle.
 
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const refs = {
-    feedbackForm: document.querySelector(".feedback-form"),
-    email: document.querySelector(".feedback-form input"),
-    textArea: document.querySelector(".feedback-form textarea"),
+    feedbackForm: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
 }
 
-window.addEventListener('DOMContentLoaded', setFormValues);
+const LOCAL_STORAGE_KEY = "feedback-form-state";
 
-const state = localStorage.getItem('feedback-form-state') 
-    ? JSON.parse(localStorage.getItem('feedback-form-state')) 
-    : {};
+refs.feedbackForm.addEventListener('submit', onSubmitBtnClick);
 
+refs.feedbackForm.addEventListener('input', throttle(onSaveFormData, 500));
 
-refs.feedbackForm.addEventListener('submit', (event) => {
+let formData = {};
+
+function onSubmitBtnClick(event) {
     event.preventDefault();
-    console.log({email: refs.email.value, message: refs.textArea.value});
+ 
+    if (refs.email.value === "" || refs.message.value === "") {
+        alert("Все поля необходимо заполнить");
+        return;
+    }
+    console.log({email: refs.email.value, message: refs.message.value});
     event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-
-});
-
-refs.textArea.addEventListener('input', throttle(handleSetStorageState, 500));
-
-refs.email.addEventListener('input', throttle(handleSetStorageState, 500));
-
-function handleSetStorageState(event) {
-    state[event.target.name] = event.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(state));
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
 
-function setFormValues() {
+function onSaveFormData(event) {
+    formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
 
-    if(!localStorage.getItem('feedback-form-state'))
-    return;
+onLoadFromData();
 
-    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+function onLoadFromData() {
+    const parsedFormData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 
-    if(savedData.email) {
-        refs.email.value = savedData.email;
-    }
+    if (!parsedFormData) return;
 
-    if(savedData.message) {
-        refs.textArea.value = savedData.message;
-    }
+    refs.email.value = parsedFormData.email || '';
+    refs.message.value = parsedFormData.message || '';
 }
